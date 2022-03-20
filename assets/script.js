eaa76089a57e4dcfda0b2c0c8b2152d778f7537c
Isaak Morales <ice.isaak97@gmail.com>
2022-03-20 17:11:08 -0400

    Got the webpage to dissappear when you hit the start quiz button

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,28 @@
 var startQuiz = document.querySelector("#start")
-var Questions = document.querySelector("Questions")
-var 
+var Questions = document.querySelector("nav")
+var header = document.querySelector("header")
+var questionOption = document.querySelector("article")
+var time = document.querySelector("time")
+var questionAppear = document.querySelector(".Questions")
+var selectA = document.querySelector("#buttonA") 
+var selectB = document.querySelector("buttonB")
+var selectC = document.querySelector("buttonC")
+var selectD = document.querySelector("buttonD")
+var secondsLeft =75;
 
 //TODO WHEN I click the start button
 //TODO THEN a timer starts and I am presented with a question
 
-startQuiz.addEventListener("click",function() {
+startQuiz.addEventListener("click", function() {
     console.log("click")
-
+    header.style.display = "none";
+    questionOption.classList.remove("article")
+    showQuestions();
 })
+
 //TODO  WHEN I answer a question
 //TODO THEN I am presented with another question
+
 
 
 //TODO WHEN I answer a question incorrectly
@@ -35,7 +47,7 @@ startQuiz.addEventListener("click",function() {
 function setTime() {
     var timeInterval = setInterval(function(){
         secondsLeft--;
-        timeElement.textContent = "Time-Limit: " + secondsLeft;
+        time.textContent = "Time-Limit: " + secondsLeft;
         if (secondsLeft < 0) {
             clearInterval(timeInterval)
         }
