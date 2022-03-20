d23e831297174617faa08c6789663ce0475752c2
Isaak Morales <ice.isaak97@gmail.com>
2022-03-20 16:15:10 -0400

    Added the timer function, and added my TODOS

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,44 @@
-//*
+var startQuiz = document.querySelector("#start")
+var Questions = document.querySelector("Questions")
+var 
+
+//TODO WHEN I click the start button
+//TODO THEN a timer starts and I am presented with a question
+
+startQuiz.addEventListener("click",function() {
+    console.log("click")
+
+})
+//TODO  WHEN I answer a question
+//TODO THEN I am presented with another question
+
+
+//TODO WHEN I answer a question incorrectly
+//TODO THEN time is subtracted from the clock
+
+
+//TODO WHEN all questions are answered or the timer reaches 0
+//TODO THEN the game is over
+
+
+
+//TODO WHEN the game is over
+//TODO THEN I can save my initials and my score
+
+
+
+
+
+
+//*Timer
+
+function setTime() {
+    var timeInterval = setInterval(function(){
+        secondsLeft--;
+        timeElement.textContent = "Time-Limit: " + secondsLeft;
+        if (secondsLeft < 0) {
+            clearInterval(timeInterval)
+        }
+            },1000);
+                }
+setTime();
